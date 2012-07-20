826d1c9ada638e79591409487c531658223135c2
Nicolas De Jay <nicolas.de.jay@ircm.qc.ca>
2012-07-20 13:23:42 -0400

    Implemented a primitive version of lazy MiM evaluation directly in ::MutualInformationMatrix (needs to undergo extensive re-factoring)

--- src/MutualInformationMatrix.cpp
+++ src/MutualInformationMatrix.cpp
@@ -1,19 +1,87 @@
 #include "MutualInformationMatrix.hpp"
 
-MutualInformationMatrix::MutualInformationMatrix(Matrix* const pMatrix) :
-        SymmetricMatrix(pMatrix->getColumnCount())
+MutualInformationMatrix::MutualInformationMatrix(Matrix* const pDataMatrix) :
+        SymmetricMatrix(pDataMatrix->getColumnCount()), mpDataMatrix(pDataMatrix)
 {
+    mpRankedDataMatrix = new Matrix(pDataMatrix->getRowCount(), pDataMatrix->getColumnCount());
 
+    for (unsigned int i = 0; i < mpDataMatrix->getColumnCount(); ++i)
+        (*mpRankedDataMatrix)(0, i) = std::numeric_limits<float>::quiet_NaN();
+
+    for (unsigned int i = 0; i < mColumnCount; ++i)
+        for (unsigned int j = i; j < mColumnCount; ++j)
+            SymmetricMatrix::operator()(i, j) = std::numeric_limits<float>::quiet_NaN();
 }
 
 /* virtual */
 MutualInformationMatrix::~MutualInformationMatrix()
 {
-
+    delete[] mpRankedDataMatrix;
 }
 
 /* virtual */float&
 MutualInformationMatrix::operator()(unsigned int const i, unsigned int const j)
 {
+    if (SymmetricMatrix::operator()(i, j) != SymmetricMatrix::operator()(i, j))
+    {
+        if ((*mpRankedDataMatrix)(0, i) != (*mpRankedDataMatrix)(0, i))
+            placeRanksByFeatureIndex(i);
+
+        if ((*mpRankedDataMatrix)(0, j) != (*mpRankedDataMatrix)(0, j))
+            placeRanksByFeatureIndex(j);
+
+        placeSpearmanCorrelation(i, j);
+    }
+
     return SymmetricMatrix::operator()(i, j);
+}
+
+void const
+MutualInformationMatrix::placeRanksByFeatureIndex(unsigned int const dataMatrixFeatureIndex)
+{
+    unsigned int const sample_count = mpRankedDataMatrix->getRowCount();
+    unsigned int p_order[sample_count];
+
+    for (unsigned int i = 0; i < sample_count; ++i)
+        p_order[i] = i;
+
+    std::sort(p_order, p_order + sample_count,
+            DataMatrixComparator(dataMatrixFeatureIndex, mpDataMatrix));
+
+    for (unsigned int i = 0; i < sample_count; ++i)
+        (*mpRankedDataMatrix)(p_order[i], dataMatrixFeatureIndex) = i;
+}
+
+void const
+MutualInformationMatrix::placeSpearmanCorrelation(unsigned int const i, unsigned int const j)
+{
+    float *a = &(*mpRankedDataMatrix)(0, i);
+    float *b = &(*mpRankedDataMatrix)(0, j);
+    unsigned int const sample_count = mpRankedDataMatrix->getRowCount();
+    float sum = 0.;
+
+    for (unsigned int n = 0; n < sample_count; ++n)
+    {
+        float const difference = a[n] - b[n];
+        sum += difference * difference;
+    }
+
+    float const correlation = 1
+            - ((6 * sum) / (sample_count * ((sample_count * sample_count) - 1)));
+
+    SymmetricMatrix::operator()(i, j) = -0.5 * log(1 - (correlation * correlation));
+}
+
+MutualInformationMatrix::DataMatrixComparator::DataMatrixComparator(
+        unsigned int const dataMatrixFeatureIndex, Matrix* const pDataMatrix) :
+        mDataMatrixFeatureIndex(dataMatrixFeatureIndex), mpDataMatrix(pDataMatrix)
+{
+
+}
+
+bool const
+MutualInformationMatrix::DataMatrixComparator::operator()(unsigned int const i,
+        unsigned int const j) const
+{
+    return (*mpDataMatrix)(i, mDataMatrixFeatureIndex) < (*mpDataMatrix)(j, mDataMatrixFeatureIndex);
 }
--- src/MutualInformationMatrix.hpp
+++ src/MutualInformationMatrix.hpp
@@ -1,10 +1,31 @@
 #ifndef ensemble_MutualInformationMatrix_hpp
 #define ensemble_MutualInformationMatrix_hpp
 
+#include <algorithm>
+#include <cmath>
+#include <limits>
+
 #include "SymmetricMatrix.hpp"
 
 class MutualInformationMatrix : public SymmetricMatrix
 {
+protected:
+    Matrix* const mpDataMatrix;
+    Matrix* mpRankedDataMatrix;
+
+    class DataMatrixComparator
+    {
+    private:
+        unsigned int const mDataMatrixFeatureIndex;
+        Matrix* const mpDataMatrix;
+
+    public:
+        DataMatrixComparator(unsigned int const dataMatrixFeatureIndex, Matrix* const pDataMatrix);
+
+        bool const
+        operator()(unsigned int const i, unsigned int const j) const;
+    };
+
 public:
     MutualInformationMatrix(Matrix* const pMatrix);
 
@@ -13,6 +34,12 @@ public:
 
     virtual float&
     operator()(unsigned int const i, unsigned int const j);
+
+    void const
+    placeRanksByFeatureIndex(unsigned int const dataMatrixFeatureIndex);
+
+    void const
+    placeSpearmanCorrelation(unsigned int const i, unsigned int const j);
 };
 
 #endif /* ensemble_MutualInformationMatrix_hpp */
