7d879fc549f7108f77dedc395ebb5a573385da1c
Simon Papillon-Cavanagh <simon.papillon-cavanagh@ircm.qc.ca>
2012-07-25 10:57:52 -0400

    Modified the R function to properly handle strata, weights and feature type

--- R/ensemble.R
+++ R/ensemble.R
@@ -1,7 +1,16 @@
-`build.mim` <- function(data_matrix)
+`build.mim` <- function(data_matrix, strata=NULL, weights=NULL, feature_type=NULL)
 {
+	if(is.null(statra))
+		strata <- rep.int(0, nrow(data_matrix))
+		
+	if(is.null(weights))
+		weights <- rep.int(1, nrow(data_matrix))
+		
+	if(is.null(feature_type))
+		feature_type <- rep.int(0, ncol(data_matrix))	
+	
     data_matrix <- as.matrix(data_matrix)
-    mi_matrix <- .Call("build_mim", as.vector(data_matrix), as.integer(nrow(data_matrix)),
+    mi_matrix <- .Call("build_mim", as.vector(data_matrix), as.vector(strata), as.vector(weights), as.vector(feature_type), as.integer(nrow(data_matrix)),
             as.integer(ncol(data_matrix)), "ensemble");
     mi_matrix <- matrix(mi_matrix, nrow=ncol(data_matrix), ncol=ncol(data_matrix))
     rownames(mi_matrix) <- colnames(data_matrix)
