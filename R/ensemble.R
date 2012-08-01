611e68eeb472b20bb85fb1ac813ed030acdf2b2b
Nicolas De Jay <nicolas.de.jay@ircm.qc.ca>
2012-08-01 12:46:35 -0400

    Adapted R package settings to new standards

--- R/ensemble.R
+++ R/ensemble.R
@@ -11,9 +11,8 @@
 	if (is.null(feature_types))
 		feature_types <- rep.int(0, ncol(data))	
 	
-    mi_matrix <- .Call("build_mim", as.vector(data), as.vector(strata), as.vector(weights),
-            as.vector(feature_types), as.integer(nrow(data)), as.integer(ncol(data)),
-            as.integer(length(unique(strata))), "ensemble");
+    mi_matrix <- .Call(build_mim, as.vector(data), as.vector(strata), as.vector(weights), as.vector(feature_types),
+            as.integer(nrow(data)), as.integer(ncol(data)), as.integer(length(unique(strata))));
     mi_matrix <- matrix(mi_matrix, nrow=ncol(data), ncol=ncol(data))
     rownames(mi_matrix) <- colnames(data)
     colnames(mi_matrix) <- colnames(data)
@@ -21,10 +20,12 @@
     return(mi_matrix)
 }
 
-`filter.mRMR` <- function(levels=NULL, data=NULL, strata=NULL, weights=NULL, feature_types=NULL,
-        mim=NULL, target_feature_index=NULL)
+`filter.mRMR` <- function(levels, data=NULL, strata=NULL, weights=NULL, feature_types=NULL, mim=NULL,
+        target_feature_index=NULL)
 {
-    if (is.null(mim)) # filter_mRMR_with_data
+    wrap <- function(i) t(matrix(i[length(i):1], nrow=length(levels), ncol=length(i)/length(levels)))
+    
+    if (is.null(mim))
     {
         data <- as.matrix(data)
         
@@ -37,23 +38,20 @@
         if (is.null(feature_types))
             feature_types <- rep.int(0, ncol(data))
         
-        mRMR_tree <- .Call("filter_mRMR_with_data", as.vector(levels), data, as.vector(strata),
-                as.vector(weights), as.vector(feature_types), nrow(data), ncol(data),
-                as.integer(length(unique(strata))), as.integer(target_feature_index) - 1)
+        mRMR_tree <- .Call(filter_mRMR_with_data, as.vector(levels), data, as.vector(strata), as.vector(weights),
+                as.vector(feature_types), nrow(data), ncol(data), as.integer(length(unique(strata))),
+                as.integer(target_feature_index) - 1)
     }
-    else if (is.null(data)) # filter_mRMR_with_mim
+    else if (is.null(data))
     {
         mim <- as.matrix(mim)
         
-        mRMR_tree <- .Call("filter_mRMR_with_mim", as.vector(levels), mim, ncol(mim),
-                as.integer(target_feature_index) - 1)   	
-		
+        mRMR_tree <- .Call(filter_mRMR_with_mim, as.vector(levels), mim, ncol(mim),
+                as.integer(target_feature_index) - 1)
     }
 	
-	mRMR_tree$scores <- t(matrix(mRMR_tree$scores[length(mRMR_tree$scores):1], nrow=length(levels),
-					ncol=length(mRMR_tree$scores)/length(levels)))
-	mRMR_tree$paths <- t(matrix(mRMR_tree$paths[length(mRMR_tree$paths):1], nrow=length(levels),
-					ncol=length(mRMR_tree$paths)/length(levels))) + 1
-
+    mRMR_tree$paths <- wrap(mRMR_tree$paths) + 1
+	mRMR_tree$scores <- wrap(mRMR_tree$scores)
+    
 	return (mRMR_tree)
 }
