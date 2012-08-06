fe0a664844b6f3b5fde0705c97f4e02674bc421a
Simon Papillon-Cavanagh <simon.papillon-cavanagh@ircm.qc.ca>
2012-08-06 17:05:26 -0400

    Fixed return format of compute_causality, now returns a N x N matrix of causality coefficient pairs

--- R/correlation.R
+++ R/correlation.R
@@ -55,39 +55,41 @@
 		solutions,
 		estimator=c("pearson", "spearman", "kendall"))
 {
-	allcor <- mat.or.vec(ncol(data), ncol(data))
-	causality_coefficients <- list()
+	allcor <- matrix(ncol=ncol(data), nrow=ncol(data))
+	causality_coefficients <- matrix(ncol=ncol(data), nrow=ncol(data))
 	apply(solutions, 1, function(row) {
-				triplets <- rbind(combn(row, 2), target_index)
-				apply(triplets, 2, function(triplet){
-							i <- triplet[1]
-							j <- triplet[2]
-							if(allcor[i, target_index] == 0)
+				pairs <- combn(row, 2)
+				apply(pairs, 2, function(pair){
+							i <- pair[1]
+							j <- pair[2]
+							if(is.na(causality_coefficients[i, j]))
 							{
-								r <- cor(data[,i], data[,target_index], method=estimator)
-								allcor[i, target_index] <<- r
-								allcor[target_index, i] <<- r
+								if(is.na(allcor[i, target_index]))
+								{
+									r <- cor(data[,i], data[,target_index], method=estimator)
+									allcor[i, target_index] <<- r
+									allcor[target_index, i] <<- r
+								}
+								if(is.na(allcor[i, j]))
+								{
+									r <- cor(data[,i], data[,j], method=estimator)
+									allcor[i, j] <<- r
+									allcor[j, i] <<- r
+								}
+								if(is.na(allcor[j, target_index]))
+								{
+									r <- cor(data[,j], data[,target_index], method=estimator)
+									allcor[target_index, j] <<- r
+									allcor[j, target_index] <<- r
+								}
+								causality <- -1/2 * log(((1 - allcor[i, j]^2) * (1 - allcor[i, target_index]^2)
+													* (1 - allcor[j, target_index]^2)) / (1 + 2 * allcor[i, j] * allcor[i, target_index] 
+													* allcor[j, target_index] - allcor[i, j]^2 - allcor[i, target_index]^2 - 
+													allcor[j, target_index]^2))
+								causality_coefficients[i, j] <<- causality
+								causality_coefficients[j, i] <<- causality
 							}
-							if(allcor[i, j] == 0)
-							{
-								r <- cor(data[,i], data[,j], method=estimator)
-								allcor[i, j] <<- r
-								allcor[j, i] <<- r
-							}
-							if(allcor[j, target_index] == 0)
-							{
-								r <- cor(data[,j], data[,target_index], method=estimator)
-								allcor[target_index, j] <<- r
-								allcor[j, target_index] <<- r
-							}
-							causality <- -1/2 * log(((1 - allcor[i, j]^2) * (1 - allcor[i, target_index]^2)
-												* (1 - allcor[j, target_index]^2)) / (1 + 2 * allcor[i, j] * allcor[i, target_index] 
-												* allcor[j, target_index] - allcor[i, j]^2 - allcor[i, target_index]^2 - 
-												allcor[j, target_index]^2))
-							causality_coefficients[i] <<- append(causality_coefficients[i], causality)
-							causality_coefficients[j] <<- append(causality_coefficients[j], causality)
-							print (paste(i, causality_coefficients[i]))
-			})
-	})
-	#return(causality_coefficients)
+				})
+		})
+	return(causality_coefficients)
 }
