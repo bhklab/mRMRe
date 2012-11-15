243343f2e2a0ec4ab46cc35f99b643165a91476b
Benjamin Haibe-Kains <bhaibeka@ircm.qc.ca>
2012-11-15 09:00:21 -0500

    Update test/test.R
    added a few tests

--- test/test.R
+++ test/test.R
@@ -84,9 +84,9 @@ causality(filter_2)
 visualize(network)
 
 ## test for large network (1000 genes)
-## install new version of the package if needed
+## install new version of the package if needed on gen01
 library(devtools)
-install_github("mRMRe", username="bhaibeka", branch="master")
+install_github("mRMRe", username="bhaibeka", ref="master")
 system("chmod -R 775 /stockage/Laboratoires/HAI/Rlib")
 
 
@@ -107,4 +107,28 @@ print(table(adjacencyMatrix(netw)))
 pdf("temp.pdf", width=14, height=14)
 visualize(netw)
 dev.off()
+
+
+# Generate the basic stuff
+data(cgps)
+dd <- mRMR.data(data=data.frame(cgps.ge))
+netw <- new("mRMRe.Network", data = dd, target_indices = c(1, 2), levels = c(1, 1, 1), layers = 1)
+filter <- new("mRMRe.Filter", data = dd, target_indices = c(1, 2), levels = c(1,1,1))
+
+# Create all 3 mim and remove NA's
+network_mim <- mim(netw)
+indices <- !is.na(network_mim)
+network_mim <- network_mim[indices]
+filter_mim <- mim(filter)[indices]
+data_mim <- mim(dd)[indices]
+
+# Compare
+quantile(network_mim)
+quantile(filter_mim)
+quantile(data_mim)
+
+# Causality
+causality(filter) # a list of causality scores for each solution
+causality(filter, merge=T) # a single vector of causality scores that is the min value between all solutions
+causality(netw) # network-wide minimal causality scores
 
